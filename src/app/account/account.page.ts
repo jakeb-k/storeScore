83ddde5bfcb2ea2a118e4c438d77ca1911f5622d
jakeb-k <jakeb.knowles@griffithuni.edu.au>
2023-05-01 17:08:25 +1000

    final styling update

--- src/app/account/account.page.ts
+++ src/app/account/account.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../models/user.model';
 import { ActivatedRoute, Router } from '@angular/router';
+import { formatNumber } from '@angular/common';
 
 @Component({
   selector: 'app-account',
